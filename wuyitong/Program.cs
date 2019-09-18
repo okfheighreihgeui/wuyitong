2120ee8998bb82631d80fcabf08849b63c1aebf6
轶桐 吴 <3420675312@qq.com>
2019-09-18 15:03:28 +0200

    fertig

--- wuyitong/Program.cs
+++ wuyitong/Program.cs
@@ -11,7 +11,14 @@ namespace wuyitong
         static void Main(string[] args)
         {
             string message = "hello world";
-            Console.WriteLine(message);
+            
+
+
+            int ganzeZahl = -25463;
+            double kommaZahl = 2.15896;
+            bool wahrheitswert = true;
+
+            Console.WriteLine(message+ ganzeZahl+ kommaZahl+ wahrheitswert);
         }
     }
 }
